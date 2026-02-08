3303af930e5c4eaf68f5077ecaa8c486707a21ee
shubham dubey <shubhamd1109@gmai.com>
2026-02-08 12:35:55 +0530

    Added required dependency in pom and setters and getters in EmailRequest.java

--- ai-mail-generator-bc/src/main/java/com/email/generator/dto/EmailRequest.java
+++ ai-mail-generator-bc/src/main/java/com/email/generator/dto/EmailRequest.java
@@ -8,4 +8,20 @@ import lombok.Setter;
 public class EmailRequest {
     private String emailContent;
     private String tone;
+
+    public String getEmailContent() {
+        return emailContent;
+    }
+
+    public void setEmailContent(String emailContent) {
+        this.emailContent = emailContent;
+    }
+
+    public String getTone() {
+        return tone;
+    }
+
+    public void setTone(String tone) {
+        this.tone = tone;
+    }
 }
